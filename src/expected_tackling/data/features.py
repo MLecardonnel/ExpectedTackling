fe5b985747328f5f0f2823587c5c1bc6b7f4db81
MaximeLecardonnel6x7 <mlecardo@hotmail.fr>
2023-11-03 17:31:52 +0100

    Fix angle between players and creation blockers features.

--- src/expected_tackling/data/features.py
+++ src/expected_tackling/data/features.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def create_target(visualization_tracking_data, tackles):
@@ -22,7 +23,7 @@ def _compute_distance_between_players(x1, y1, x2, y2):
 
 def _compute_angle_between_players(x1, y1, x2, y2):
     angle = np.arctan2(x2 - x1, y2 - y1)
-    return np.degrees(angle)
+    return (np.degrees(angle) + 360) % 360
 
 
 def _compute_distance_to_nearest_sideline(y, field_width=53.3):
@@ -40,8 +41,31 @@ def _compute_distance_to_endzone(x, playDirection, field_length=120):
     return distance
 
 
-def _compute_sample_features(sample, ball_carrier):
+def _compute_blockers_features(sample, frame_blockers, nb_blockers=3):
+    columns_to_suffix = ["s", "a", "dis", "o", "dir"]
+    columns_suffix_dict = {col: col + "_blocker" for col in columns_to_suffix}
+
+    frame_blockers["distance_to_blocker"] = frame_blockers.apply(
+        lambda x: _compute_distance_between_players(sample["x"], sample["y"], x["x"], x["y"]), axis=1
+    )
+    frame_blockers["direction_to_blocker"] = frame_blockers.apply(
+        lambda x: _compute_angle_between_players(sample["x"], sample["y"], x["x"], x["y"]), axis=1
+    )
+    frame_blockers = frame_blockers.sort_values(by="distance_to_blocker", ascending=True).reset_index()
+    frame_blockers = frame_blockers[columns_to_suffix + ["distance_to_blocker", "direction_to_blocker"]].head(
+        nb_blockers
+    )
+
+    frame_blockers = frame_blockers.rename(columns=columns_suffix_dict)
+
+    blockers_features = frame_blockers.stack().rename(sample.name)
+    blockers_features.index = blockers_features.index.map(lambda x: f"{x[1]}_{x[0]+1}")
+    return blockers_features
+
+
+def _compute_sample_features(sample, ball_carrier, blockers):
     frame_ball_carrier = ball_carrier.loc[(sample["gameId"], sample["playId"], sample["frameId"])]
+    frame_blockers = blockers.loc[(sample["gameId"], sample["playId"], sample["frameId"])]
 
     sample["distance_to_ball_carrier"] = _compute_distance_between_players(
         sample["x"], sample["y"], frame_ball_carrier["x"], frame_ball_carrier["y"]
@@ -50,7 +74,11 @@ def _compute_sample_features(sample, ball_carrier):
         sample["x"], sample["y"], frame_ball_carrier["x"], frame_ball_carrier["y"]
     )
 
-    return sample
+    blockers_features = _compute_blockers_features(sample, frame_blockers)
+
+    sample_features = pd.concat([sample, blockers_features])
+
+    return sample_features
 
 
 def _inverse_left_directed_plays(features_data):
@@ -79,11 +107,14 @@ def compute_features_data(targeted_data, tracking):
 
     blockers = merged_data[
         (~merged_data["is_defense"]) & (~merged_data["ball_carrier_id"].isna()) & (~merged_data["is_ball_carrying"])
-    ][["gameId", "playId", "nflId", "frameId", "x", "y", "playDirection", "s", "a", "dis", "o", "dir"]].reset_index(
-        drop=True
+    ][["gameId", "playId", "nflId", "frameId", "x", "y", "playDirection", "s", "a", "dis", "o", "dir"]].set_index(
+        ["gameId", "playId", "frameId", "nflId"]
     )
 
-    features_data = defense.apply(lambda x: _compute_sample_features(x, ball_carrier), axis=1)
+    features_data = defense.apply(lambda x: _compute_sample_features(x, ball_carrier, blockers), axis=1)
+    features_data = features_data[
+        defense.columns.to_list() + [col for col in features_data.columns if col not in defense.columns]
+    ]
 
     ball_carrier["ball_carrier_distance_to_sideline"] = ball_carrier["y"].apply(_compute_distance_to_nearest_sideline)
     ball_carrier["ball_carrier_distance_to_endzone"] = ball_carrier.apply(

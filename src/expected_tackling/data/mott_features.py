6cf2f18e3a97e56312d49cd8b14b161a40dcd955
MaximeLecardonnel6x7 <mlecardo@hotmail.fr>
2023-12-04 18:23:29 +0100

    Introduces find peaks in feature engineering.

--- src/expected_tackling/data/mott_features.py
+++ src/expected_tackling/data/mott_features.py
@@ -1,12 +1,30 @@
-def _compute_group_features(group):
-    group = group.sort_values("frameId")
-    argmax = group["ott"].argmax()
-    res = group[["ott", "ball_carrier_distance_to_endzone"]].iloc[argmax]
-    res["mean_distance_to_ball_carrier_from_peak"] = group.iloc[argmax:]["distance_to_ball_carrier"].mean()
+from scipy.signal import find_peaks
+import pandas as pd
+
+
+def _find_peaks(ott):
+    peaks = find_peaks(ott.tolist() + [0], height=0.5, distance=16)[0].tolist()
+    if len(peaks) == 0:
+        peaks = [ott.argmax()]
+    return peaks
+
+
+def _compute_peak_features(group, peak):
+    res = group[["ott", "ball_carrier_distance_to_endzone"]].iloc[peak]
+    res["mean_distance_to_ball_carrier_from_peak"] = group.iloc[peak:]["distance_to_ball_carrier"].mean()
     res["ball_carrier_distance_won_to_last_frame"] = max(
         res["ball_carrier_distance_to_endzone"] - group.iloc[-1]["ball_carrier_distance_to_endzone"], 0
     )
     res = res.drop(index="ball_carrier_distance_to_endzone")
+    return res
+
+
+def _compute_group_features(group):
+    group = group.sort_values("frameId")
+    peaks = _find_peaks(group["ott"])
+    res = pd.concat([_compute_peak_features(group, peak) for peak in peaks], axis=1)
+    res = res.T.reset_index(drop=True)
+    res.index.name = "opportunityId"
     return res
 
 
@@ -27,6 +45,7 @@ def compute_mott_features_data(features_data, tackling_probability, tackles):
         right_index=True,
     )
     mott_features_data["tackle_or_assist"] = mott_features_data["tackle_or_assist"].fillna(0)
+    mott_features_data.loc[mott_features_data.index.droplevel(3).duplicated(keep="last"), "tackle_or_assist"] = 0
 
     mott_features_data = mott_features_data.merge(
         tackles[tackles["pff_missedTackle"] == 1].set_index(["gameId", "playId", "nflId"])[["pff_missedTackle"]],
@@ -35,5 +54,11 @@ def compute_mott_features_data(features_data, tackling_probability, tackles):
         right_index=True,
     )
     mott_features_data["pff_missedTackle"] = mott_features_data["pff_missedTackle"].fillna(0)
+    mott_features_data.loc[
+        (mott_features_data.index.droplevel(3).duplicated(keep=False))
+        & (~mott_features_data.index.droplevel(3).duplicated(keep="last"))
+        & (mott_features_data["tackle_or_assist"] == 1),
+        "pff_missedTackle",
+    ] = 0
 
     return mott_features_data
